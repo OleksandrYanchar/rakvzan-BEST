cb0404ee03246dfecc485558f1ba974ab494cda6
Oleksandr Yanchar <contact.oleksandryanchar@gmail.com>
2025-04-26 15:14:33 +0300

    Init

---
+++ backend/src/api/v1/ninja/urls.py
@@ -0,0 +1,10 @@
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.controller import NinjaJWTDefaultController
+from src.api.v1.ninja.establishment.handlers import EstablishmentController
+
+api = NinjaExtraAPI()
+
+api.register_controllers(
+    NinjaJWTDefaultController,
+    EstablishmentController,
+)
